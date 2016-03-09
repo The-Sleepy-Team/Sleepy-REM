b19b4823f5da3cf687b95135bd249ce24887a622
Ji Yeon <jykim6@uci.edu>
2016-03-09 01:42:58 -0800

    back-end MailRead is usable

--- HelloWorld/app/src/main/java/com/example/tyler/helloworld/MailRead.java
+++ HelloWorld/app/src/main/java/com/example/tyler/helloworld/MailRead.java
@@ -8,7 +8,11 @@ import javax.mail.*;
 import javax.activation.*;
 
 public class MailRead {
-    public static void main(String[] args) {
+    private String _sent_date;
+    private String _subject;
+    private String _content;
+
+    public MailRead() {
         Properties props = new Properties();
         props.setProperty("mail.store.protocol", "imaps");
         try {
@@ -33,11 +37,24 @@ public class MailRead {
 
             Multipart mp = (Multipart) msg.getContent();
             BodyPart bp = mp.getBodyPart(0);
-            System.out.println("SENT DATE:" + msg.getSentDate());
-            System.out.println("SUBJECT:" + msg.getSubject());
-            System.out.println("CONTENT:" + bp.getContent());
+
+            //debugging purposes
+            //System.out.println("SENT DATE:" + msg.getSentDate());
+            //System.out.println("SUBJECT:" + msg.getSubject());
+            //System.out.println("CONTENT:" + bp.getContent());
+
+            _sent_date = msg.getSentDate().toString();
+            _subject = msg.getSubject();
+            _content = msg.getContent().toString();
+
         } catch (Exception mex) {
             mex.printStackTrace();
         }
     }
+
+    //getters
+    public String getContent() {return _content;}
+    public String getSubject() {return _subject;}
+    public String getSentDate() {return _sent_date;}
+
 }
